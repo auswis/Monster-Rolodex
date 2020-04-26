e501959778feabc7c830d1ab9a10b6fce4dc3cdd
Dominic Schleheck <dominicita081@googlemail.com>
2020-04-26 20:40:20 +0200

    Updates

---
+++ precache-manifest.5087fc0d8a7b98085743906a4ac4fd45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1866f80216ef5b75bbd157420df8afee",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "ec8b799712c538a94390",
+    "url": "/Monster-Rolodex/static/css/main.f55cdfb9.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/Monster-Rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8b799712c538a94390",
+    "url": "/Monster-Rolodex/static/js/main.ea49ac95.chunk.js"
+  },
+  {
+    "revision": "2d49b9103f3e45b8c1fb",
+    "url": "/Monster-Rolodex/static/js/runtime-main.2fc03e53.js"
+  }
+]);
